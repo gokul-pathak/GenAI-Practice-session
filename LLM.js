e55df4d082ce8fded9dd6e942d532d459d56141a
Gokul Pathak <gokul_pathak@yahoo.com>
2026-02-07 20:34:41 +0545

    refactor: replace dotenv with readline-sync for user input and update chat model implementation

--- LLM.js
+++ LLM.js
@@ -1,19 +1,28 @@
-import "dotenv/config";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import readlineSync from "readline-sync";
+
+process.loadEnvFile();
 
 const ai = new GoogleGenerativeAI(process.env.API_KEY);
+const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const chat = model.startChat({
+  history: [],
+});
 
 async function main() {
-  const model = ai.getGenerativeModel({ model: "gemini-2.5-flash" });
+  const userProblem = readlineSync.question("Ask me anything --> ");
+
+  if (userProblem.toLowerCase() === "exit") return;
+
   try {
-    const result = await model.generateContent(
-      "Explain how AI works in a few words",
-    );
+    const result = await chat.sendMessage(userProblem);
     const response = await result.response;
-    console.log(response.text());
+    console.log("AI:", response.text());
+    main();
   } catch (error) {
-    console.error("Error during content generation:", error.message);
+    console.error("Error:", error.message);
   }
 }
 
-await main();
+main();
